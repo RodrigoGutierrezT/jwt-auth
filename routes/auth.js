9157aea0fbfb1b528fb73b0cb2c4076b61f3db27
Rodrigo Gutierrez <rodrigo644@gmail.com>
2022-02-24 16:34:49 -0300

    Add Joi schema validation on POST /register

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+
+// Validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+});
+
+
 router.post('/register', async (req,res) => {
+
+    // validate data before making a User
+    const { error } = schema.validate(req.body);
+
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
